96a6dfd7a6338cc3cf8882cfc1b01c1a484d440b
Xuan  Tuoi <tuoittx@ikong.vn>
2023-12-08 15:32:41 +0700

    fix: dashboard/page.tsx has an invalid default export ERROR

--- src/app/[lang]/admin/dashboard/page.tsx
+++ src/app/[lang]/admin/dashboard/page.tsx
@@ -5,18 +5,14 @@ import { Locale } from '../../../../../i18n-config'
 import Loading from '../../loading'
 
 const DashboardPage = async ({
-	children,
 	params: { lang }
 }: {
-	children: React.ReactNode
 	params: { lang: Locale }
 }) => {
 	const dictionary = await getDictionary(lang)
 	return (
 		<Suspense fallback={<Loading />}>
-			<DashboardContainer dictionary={dictionary}>
-				{children}
-			</DashboardContainer>
+			<DashboardContainer dictionary={dictionary} />
 		</Suspense>
 	)
 }
