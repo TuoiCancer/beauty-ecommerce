82bcae285edeb347c765a0ef3e4bbbe6eaf7dfc7
Xuan  Tuoi <tuoittx@ikong.vn>
2023-12-15 22:00:12 +0700

    feat: update UI in setting user and add animate icon in header

--- src/components/settings/Sidebar.tsx
+++ src/components/settings/Sidebar.tsx
@@ -11,42 +11,70 @@ const SidebarSetting = ({ dictionary, lang }: any) => {
 	const listSidebar = [
 		{
 			id: 'user-info',
-			text: 'User Info',
-			icon: PersonOutlineIcon,
-			href: '/' + lang + '/user/settings/infor'
+			text: 'Profile',
+			href: '/' + lang + '/user/settings/infor',
+			img: '/img/user.png'
 		},
 		{
 			id: 'notify',
 			text: 'Notifications',
-			icon: NotificationsNoneIcon,
-			href: '/' + lang + '/user/settings/notifications'
+			href: '/' + lang + '/user/settings/notifications',
+			img: '/img/bell.png'
 		},
 		{
 			id: 'authen',
 			text: 'Password and Security',
-			icon: SecurityIcon,
-			href: '/' + lang + '/user/settings/security'
+			href: '/' + lang + '/user/settings/security',
+			img: '/img/shield.png'
 		}
 	]
+
 	return (
 		<Box
 			id='sidebar'
 			sx={{
 				borderRight: '2px solid #f2f2f2',
-				gridColumn: { xs: '1 / 2', md: '1 / 2' }
+				gridColumn: { xs: '1 / 2', md: '1 / 2' },
+				boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.1)',
+				height: '100%',
+				padding: '120px 0 120px 0',
+				maxWidth: '600px'
 			}}
 		>
-			<Typography
-				variant='h2'
+			<Box
 				sx={{
-					fontFamily: 'var(--font-family)',
-					fontWeight: '600',
-					fontSize: '36px',
-					marginBottom: '32px'
+					padding: '24px 36px',
+					'&::after': {
+						content: '""',
+						display: 'block',
+						width: '20%',
+						height: '1px',
+						backgroundColor: '#98A8BB',
+						marginTop: '24px'
+					}
 				}}
 			>
-				{dictionary['Setting']['sidebar']['title']}
-			</Typography>
+				<Typography
+					sx={{
+						fontFamily: 'Montserrat, sans-serif',
+						fontWeight: '600',
+						fontSize: '22px',
+						mb: '16px'
+					}}
+				>
+					Setting
+				</Typography>
+				<Typography
+					sx={{
+						fontFamily: 'Montserrat, sans-serif',
+						fontWeight: '400',
+						fontSize: '14px',
+						color: '#C9D2DA'
+					}}
+				>
+					This page allow you to change configuration what you have made
+				</Typography>
+			</Box>
 			{listSidebar.map((item, index: number) => {
 				return (
 					<SidebarInfoItem key={index} item={item} dictionary={dictionary} />
--- src/components/settings/information/SidebarInfoItem.tsx
+++ src/components/settings/information/SidebarInfoItem.tsx
@@ -1,4 +1,5 @@
 'use client'
+import ImageItem from '@/components/base/ImageItem'
 import { Box, Typography } from '@mui/material'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -23,28 +24,50 @@ const SidebarInfoItem = ({ item, dictionary }: any) => {
 			<Box
 				sx={{
 					display: 'flex',
-					alignItems: 'center',
-					p: '12px 0',
-					color: active ? '#000' : '#9b9b9b',
-					'& svg': {
-						fontSize: '32px',
-						mr: '12px'
-					},
+					alignItems: 'flex-start',
+					p: '32px 0 32px 32px',
+					backgroundColor: active ? '#FdFdFd' : 'transparent',
+					color: active ? '#000' : '#BAC5D2',
+					borderTop: '1px solid #F2F2F2',
 					'&:hover': {
 						cursor: 'pointer'
 					}
 				}}
 			>
-				<item.icon />
-				<Typography
-					variant='h6'
-					sx={{
-						fontFamily: 'var(--font-family)',
-						fontWeight: active ? '600' : '400'
+				<ImageItem
+					style={{
+						width: active ? '36px' : '32px',
+						height: active ? '36px' : '32px',
+						marginRight: '12px',
+						filter: active ? 'opacity(1)' : 'opacity(0.5)'
 					}}
-				>
-					{dictionary['Setting']['sidebar'][item.id]}
-				</Typography>
+					imgSrc={item.img}
+				/>
+				<Box>
+					<Typography
+						variant='h5'
+						sx={{
+							fontFamily: 'var(--font-family)',
+							fontWeight: active ? '600' : '400',
+							fontSize: active ? '20px' : '16px',
+							pb: '8px',
+							color: active ? '#02cbc5' : '#BAC5D2'
+						}}
+					>
+						{item.text}
+					</Typography>
+					<Typography
+						variant='h5'
+						sx={{
+							color: '#BAC5D2',
+							fontSize: '12px',
+							fontWeight: '400',
+							fontFamily: 'var(--font-family)'
+						}}
+					>
+						Change name, ava, title and bio
+					</Typography>
+				</Box>
 				<Box
 					sx={{
 						width: '4px',
